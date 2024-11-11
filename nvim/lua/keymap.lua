2ca69ecb641072bfc1c36d13d12c34124104c9dd
Lacy <me@lacylefae.net>
2024-11-11 11:35:21 -0600

    nvim: j and k mapped to gj and gk respectively

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -5,8 +5,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set('n', '<leader>ve', ':$tabedit $MYVIMRC<cr>')
-vim.keymap.set('n', '<leader>vr', ':source $MYVIMRC<cr>')
+-- ease of use
 vim.keymap.set('n', ';', ':')
 vim.keymap.set('v', ';', ':')
 vim.keymap.set('n', 'gf', '<cmd>edit <cfile><cr>')
@@ -15,13 +14,21 @@ vim.keymap.set('n', 'H', '^')
 vim.keymap.set('n', 'U', '<C-r>')
 vim.keymap.set('v', 'L', '$')
 vim.keymap.set('v', 'H', '^')
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+-- indentation
 vim.keymap.set('n', '<C-l>', '>>')
 vim.keymap.set('n', '<C-h>', '<<')
 vim.keymap.set('v', '<C-l>', '>gv')
 vim.keymap.set('v', '<C-h>', '<gv')
 vim.keymap.set('i', '<C-l>', '<C-t>')
 vim.keymap.set('i', '<C-h>', '<C-d>')
+-- functional
 vim.keymap.set('n', '<F4>', '<cmd>noh<cr>', { silent = true })
+
+-- init.lua edit
+vim.keymap.set('n', '<leader>ve', ':$tabedit $MYVIMRC<cr>')
+vim.keymap.set('n', '<leader>vr', ':source $MYVIMRC<cr>')
 
 -- For Lazy
 vim.keymap.set('n', '<leader>ll', ':Lazy<cr>')
